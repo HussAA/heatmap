470bc909d7c112a9f7a16fa3dad9d716aac06871
Hussaa <mah.hussaina@gmail.com>
2023-07-05 09:10:01 -0400

    added comments

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from "react";
 
 const IndexPage = () => {
-  const [customers, setCustomers] = useState([]);
+  const [customers, setCustomers] = useState([]); // 1st value current state, 2nd is to update our state
 
   useEffect(() => {
-    fetch("http://localhost:3000/customers")
-      .then((res) => res.json())
-      .then((data) => setCustomers(data));
-  }, []);
+    fetch("http://localhost:3000/customers") // fetch data from the URL
+      .then((res) => res.json()) // takes res from server converts to json
+      .then((data) => setCustomers(data)); // takes the converted data and update our state
+  }, []); // empty array to run this once
 
   return (
     <div>
       <h1>Customer List </h1>
       <br />
       <ul>
-        {customers.map((item) => (
+        {customers.map((item) => ( 
           <li key={item.postal_code}>{item.postal_code}</li>
         ))}
       </ul>
